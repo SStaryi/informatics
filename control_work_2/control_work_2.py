7c0d37737d3395c10c6359cd21e1bf44c0cc7c67
SStaryi <acsh114@gmail.com>
2023-10-13 11:31:34 +0300

    control work 2 completed

---
+++ control_work_2/control_work_2.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+
+def formula(a, b, c):
+    return (int)(not (b or c) and not (a and c))
+
+
+def truth_table_for_3():
+    print("A\tB\tC\tF(A, B, C)")
+
+    for a, b, c in product([0, 1], repeat=3):
+        print(a, "\t", b, "\t", c, "\t", formula(a, b, c))
